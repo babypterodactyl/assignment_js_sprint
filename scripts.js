667b72764a50bf63c2c6890d3ddf124654a372d6
babypterodactyl <sasha@babypterodactyl.com>
2017-04-14 21:39:26 -0500

    solved second test

--- scripts.js
+++ scripts.js
@@ -1,35 +1,37 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){
-    var input = [2,4,2,1,5,2,5]
-    return Math.max(...input)
-  },
+    largestEl: function() {
+        var input = [2, 4, 2, 1, 5, 2, 5]
+        return Math.max(...input)
+    },
 
-  reversed: function(input){
-    
+    reversed: function(input) {
+        input = "I am a special string!"
+        var secondString = input.split("")
+        var backwardsArray = secondString.reverse();
+        var finalArray = backwardsArray.join("");
+        return finalArray;
+    },
 
+    loudSnakeCase: function() {
+        // your code here
+    },
 
-  },
+    compareArrays: function() {
+        // your code here (replace the return)
+        return "Finish compareArrays first!"
+    },
 
-  loudSnakeCase: function(){
-    // your code here
-  },
+    fizzBuzz: function() {
+        // your code here
+    },
 
-  compareArrays: function(){
-    // your code here (replace the return)
-    return "Finish compareArrays first!"
-  },
+    myMap: function() {
+        // your code here
+    },
 
-  fizzBuzz: function(){
-    // your code here
-  },
-
-  myMap: function(){
-    // your code here
-  },
-
-  primes: function(){
-    // your code here
-  },
+    primes: function() {
+        // your code here
+    },
 }
